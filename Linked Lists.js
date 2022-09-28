e4fac4b95781a54401917bd35dd3929763701e5a
mmaksi <markmaksi@outlook.com>
2022-09-28 11:22:34 +0300

    Implementing a singly linked list with its methods

--- Linked Lists.js
+++ Linked Lists.js
@@ -8,19 +8,19 @@ class Node {
 }
 
 class LinkedList {
-  // Building the head node
   constructor(value) {
     this.head = {
       value,
       next: null,
     };
     this.tail = this.head;
-    this.length = 1;
   }
 
   append(value) {
     const newNode = new Node(value);
+    // Changing the pointer of the current tail
     this.tail.next = newNode;
+    // Setting the new tail
     this.tail = newNode;
     this.length++;
     return this;
@@ -33,8 +33,51 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  printList() {
+    const output = [];
+    let currentNode = this.head;
+    while (currentNode) {
+      output.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return output;
+  }
+
+  insert(index, value) {
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  delete(index) {
+    const leader = this.traverseToIndex(index - 1);
+    const node = leader.next;
+    const holdingPointer = node.next;
+    leader.next = holdingPointer;
+    this.length--;
+    return this.printList();
+  }
+
+  traverseToIndex(index) {
+    const values = this.printList();
+    let currentNode = this.head;
+    while (currentNode.value !== values[index]) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
+myLinkedList.append(8);
+// myLinkedList.printList();
+myLinkedList.insert(2, 22);
+// myLinkedList.printList();
+myLinkedList.delete(2);
